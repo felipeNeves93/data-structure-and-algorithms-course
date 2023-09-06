5dafb69f992e8126e4337b10d8d241d8070bc1f5
Felipe Neves <felipe.alneves@outlook.com>
2023-09-06 17:01:43 -0300

    Added removeLastLiked List tests

--- src/main/java/com/content/linkedlist/implementation/Main.java
+++
@@ -1,9 +0,0 @@
-package com.content.linkedlist.implementation;
-
-public class Main {
-    public static void main(String... args) {
-        var myLinkedList = new LinkedList(1);
-
-        myLinkedList.get(0);
-    }
-}
---
+++ src/main/test/linkedList/LinkedListRemoveLastTests.java
@@ -0,0 +1,32 @@
+package linkedList;
+
+import com.content.linkedlist.implementation.LinkedList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class LinkedListRemoveLastTests {
+
+    @Test
+    void whenRemoveFromEmptyListThenShouldReturnNull() {
+        var linkedList = new LinkedList();
+        var value = linkedList.removeLast();
+
+        assertNull(value);
+    }
+
+    @Test
+    void whenRemoveLastElementThenShouldReturnPreviousElement() {
+        var linkedList = new LinkedList(1);
+        linkedList.append(2);
+        linkedList.append(3);
+
+        final var removedElement = linkedList.removeLast();
+        final var expectedTail = 2;
+        final var expectedRemovedElement = 3;
+
+        assertEquals(expectedRemovedElement, removedElement.value);
+        assertEquals(expectedTail, linkedList.getTail().value);
+    }
+}
