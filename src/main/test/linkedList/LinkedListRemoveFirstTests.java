e70f70016af0e2f05dce099e54de3e49170e7515
Felipe Neves <felipe.alneves@outlook.com>
2023-09-06 21:50:28 -0300

    Added removeFirst Linked List tests

---
+++ src/main/test/linkedList/LinkedListRemoveFirstTests.java
@@ -0,0 +1,34 @@
+package linkedList;
+
+import com.content.linkedlist.implementation.LinkedList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class LinkedListRemoveFirstTests {
+
+    @Test
+    void whenRemoveFromEmptyListThenShouldReturnNull() {
+        var linkedList = new LinkedList();
+        var value = linkedList.removeFirst();
+
+        assertNull(value);
+    }
+
+    @Test
+    void whenRemoveFirstElementThenShouldReturnPreviousElement() {
+        var linkedList = new LinkedList(1);
+        linkedList.append(2);
+        linkedList.append(3);
+
+        final var removedElement = linkedList.removeFirst();
+        final var expectedTail = 3;
+        final var expectedRemovedElement = 1;
+        final var expectedHeadElement = 2;
+
+        assertEquals(expectedRemovedElement, removedElement.value);
+        assertEquals(expectedTail, linkedList.getTail().value);
+        assertEquals(expectedHeadElement, linkedList.getHead().value);
+    }
+}
